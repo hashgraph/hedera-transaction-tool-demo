ca8ad1bf6b04bae811a39eb3e4ca8957eae3c6b8
David Sergio Matusevich <davidmatusevich@swirlds.com>
2021-07-26 10:02:05 -0500

    initial check
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>

--- tools-core/src/test/java/com/hedera/hashgraph/client/core/transactions/ToolTransferTransactionTest.java
+++ tools-core/src/test/java/com/hedera/hashgraph/client/core/transactions/ToolTransferTransactionTest.java
@@ -19,25 +19,39 @@
 package com.hedera.hashgraph.client.core.transactions;
 
 import com.google.gson.JsonObject;
+import com.hedera.hashgraph.client.core.action.GenericFileReadWriteAware;
 import com.hedera.hashgraph.client.core.constants.Constants;
 import com.hedera.hashgraph.client.core.constants.JsonConstants;
+import com.hedera.hashgraph.client.core.enums.NetworkEnum;
 import com.hedera.hashgraph.client.core.exceptions.HederaClientException;
 import com.hedera.hashgraph.client.core.json.Timestamp;
+import com.hedera.hashgraph.client.core.security.Ed25519KeyStore;
+import com.hedera.hashgraph.client.core.utils.CommonMethods;
 import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.AccountInfo;
+import com.hedera.hashgraph.sdk.AccountInfoQuery;
 import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.PrecheckStatusException;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.ReceiptStatusException;
+import com.hedera.hashgraph.sdk.TransactionReceipt;
+import com.hedera.hashgraph.sdk.TransactionResponse;
 import com.hedera.hashgraph.sdk.TransferTransaction;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.KeyStoreException;
+import java.util.concurrent.TimeoutException;
 
 import static com.hedera.hashgraph.client.core.helpers.TestHelpers.getJsonInputCA;
 import static com.hedera.hashgraph.client.core.helpers.TestHelpers.getJsonInputCT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class ToolTransferTransactionTest {
+class ToolTransferTransactionTest implements GenericFileReadWriteAware {
 
 	private final long sender = 2;
 	private final long receiver = 50;
@@ -76,6 +90,36 @@ class ToolTransferTransactionTest {
 		AccountCreateTransaction create = (AccountCreateTransaction) transaction.getTransaction();
 
 		assertEquals(Hbar.fromTinybars(50), create.getInitialBalance());
+
+	}
+
+	@Test
+	void receiverRequiredAccounts_test() throws KeyStoreException, PrecheckStatusException, TimeoutException,
+			ReceiptStatusException, HederaClientException {
+		var generalPrivateKey = PrivateKey.generate();
+		var generalPublicKey = generalPrivateKey.getPublicKey();
+		var client = CommonMethods.getClient(NetworkEnum.INTEGRATION);
+		var keyStore =
+				Ed25519KeyStore.read(Constants.TEST_PASSWORD.toCharArray(), "src/test/resources/Keys/genesis.pem");
+		var genesisKey = PrivateKey.fromBytes(keyStore.get(0).getPrivate().getEncoded());
+		client.setOperator(new AccountId(0, 0, 2), genesisKey);
+
+
+		for (int i = 0; i < 2; i++) {
+			TransactionResponse transactionResponse = new AccountCreateTransaction()
+					// The only _required_ property here is `key`
+					.setKey(generalPublicKey)
+					.setInitialBalance(Hbar.fromTinybars(1000000000))
+					.execute(client);
+
+			// This will wait for the receipt to become available
+			TransactionReceipt receipt = transactionResponse.getReceipt(client);
+
+			AccountId newAccountId = receipt.accountId;
+			assert newAccountId != null;
+			AccountInfo info = new AccountInfoQuery().setAccountId(newAccountId).execute(client);
+			writeBytes("src/test/resources/AccountInfos/" + newAccountId + ".info", info.toBytes());
+		}
 
 	}
 }
