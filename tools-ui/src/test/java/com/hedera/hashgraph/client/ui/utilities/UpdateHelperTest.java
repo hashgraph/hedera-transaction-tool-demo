2a514a072b4be74fc6e9cd41dc26eb2ec56da0af
David Sergio Matusevich <davidmatusevich@swirlds.com>
2022-03-30 10:38:09 -0500

    same
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>

--- tools-ui/src/test/java/com/hedera/hashgraph/client/ui/utilities/UpdateHelperTest.java
+++ tools-ui/src/test/java/com/hedera/hashgraph/client/ui/utilities/UpdateHelperTest.java
@@ -24,6 +24,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,6 +41,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Ignore
 class UpdateHelperTest {
 
 	private static final Logger logger = LogManager.getLogger(UpdateHelperTest.class);
