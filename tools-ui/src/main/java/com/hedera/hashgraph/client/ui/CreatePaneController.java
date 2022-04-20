bc1a748f53445950544a371c89b84c847712e536
David Matusevich <42348838+DSMatusevich@users.noreply.github.com>
2022-04-20 14:56:21 -0500

    Fixed an issue with transfer tables (#295)
    * fixed an issue with transfer tables
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * fix for missing network
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Changed test
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>

--- tools-ui/src/main/java/com/hedera/hashgraph/client/ui/CreatePaneController.java
+++ tools-ui/src/main/java/com/hedera/hashgraph/client/ui/CreatePaneController.java
@@ -22,6 +22,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.hashgraph.client.core.action.GenericFileReadWriteAware;
+import com.hedera.hashgraph.client.core.enums.NetworkEnum;
 import com.hedera.hashgraph.client.core.enums.SetupPhase;
 import com.hedera.hashgraph.client.core.exceptions.HederaClientException;
 import com.hedera.hashgraph.client.core.fileservices.FileAdapterFactory;
@@ -1080,7 +1081,8 @@ public class CreatePaneController implements GenericFileReadWriteAware {
 		final AccountAmountStrings newTransaction;
 		newTransaction = new AccountAmountStrings(account.getText(), stripHBarFormat(amount.getText()));
 
-		final var status = parseAddress(newTransaction.getStrippedAccountID()).getStatus();
+		final var status = parseAddress(NetworkEnum.asLedger(controller.getCurrentNetwork()).toBytes(),
+				newTransaction.getStrippedAccountID()).getStatus();
 		if (status.equals(parseStatus.BAD_CHECKSUM) || status.equals(parseStatus.BAD_FORMAT)) {
 			account.setStyle(RED_BORDER_STYLE);
 			account.selectAll();
@@ -2597,7 +2599,7 @@ public class CreatePaneController implements GenericFileReadWriteAware {
 			account = "0.0." + account;
 		}
 
-		final var parsedAddress = parseAddress(account);
+		final var parsedAddress = parseAddress(NetworkEnum.asLedger(controller.getCurrentNetwork()).toBytes(), account);
 		switch (parsedAddress.getStatus()) {
 			case BAD_FORMAT:
 				textField.setStyle(TEXTFIELD_ERROR);
