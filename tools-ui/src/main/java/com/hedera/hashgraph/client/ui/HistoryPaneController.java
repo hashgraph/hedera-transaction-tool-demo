07d9202f20658d62a849048e5b89184901ff91c7
David Matusevich <42348838+DSMatusevich@users.noreply.github.com>
2022-04-27 09:36:44 -0500

    Issues found on review (#307)
    * incremental checkin
    
    - fixed typo (#305)
    - software update no longer shows re-sign button (#297)
    - added table refresh after adding an element (#304)
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Added limits to date pickers in History filters (#306)
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Fixed code smells
    
    - Added @Override annotations
    - Changed the title of the expiration column (#298)
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Removed title from History cards (#299)
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Removed flaky test
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>
    
    * Bumped version
    
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>

--- tools-ui/src/main/java/com/hedera/hashgraph/client/ui/HistoryPaneController.java
+++ tools-ui/src/main/java/com/hedera/hashgraph/client/ui/HistoryPaneController.java
@@ -42,6 +42,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -88,8 +89,14 @@ import static com.hedera.hashgraph.client.core.constants.Constants.DEFAULT_HISTO
 import static com.hedera.hashgraph.client.core.constants.Constants.DEFAULT_SYSTEM_FOLDER;
 import static com.hedera.hashgraph.client.core.constants.Constants.HISTORY_MAP;
 import static com.hedera.hashgraph.client.core.constants.ToolTipMessages.FILTER_TOOLTIP_TEXT;
+import static com.hedera.hashgraph.client.core.enums.FileType.ACCOUNT_INFO;
+import static com.hedera.hashgraph.client.core.enums.FileType.BUNDLE;
 import static com.hedera.hashgraph.client.core.enums.FileType.COMMENT;
 import static com.hedera.hashgraph.client.core.enums.FileType.METADATA;
+import static com.hedera.hashgraph.client.core.enums.FileType.PUBLIC_KEY;
+import static com.hedera.hashgraph.client.core.enums.FileType.SOFTWARE_UPDATE;
+import static com.hedera.hashgraph.client.core.enums.FileType.UNKNOWN;
+import static com.hedera.hashgraph.client.core.enums.FileType.getType;
 import static com.hedera.hashgraph.client.ui.utilities.Utilities.parseAccountNumbers;
 import static java.nio.file.Files.deleteIfExists;
 import static javafx.beans.binding.Bindings.createObjectBinding;
@@ -242,10 +249,11 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 	public void addToHistory(final RemoteFile remoteFile) {
 		noise = false;
 		final var addition = new HistoryData(remoteFile);
+		tableList.remove(addition);
 		addition.setHistory(true);
 		historyMap.put(addition.getCode(), true);
-		tableList.remove(addition);
 		tableList.add(0, addition);
+		tableView.refresh();
 	}
 
 	/**
@@ -374,7 +382,7 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 		expirationDateColumn.setCellValueFactory(new PropertyValueFactory<>("expirationDate"));
 		expirationDateColumn.prefWidthProperty().bind(tableView.widthProperty().divide(5));
 		expirationDateColumn.setCellFactory(historyDataStringTableColumn -> setWrapping());
-		final var lastActionBox = getTitleBox("Expiration Date:", expirationDateFilterVBox);
+		final var lastActionBox = getTitleBox("Expiration:", expirationDateFilterVBox);
 		expirationDateColumn.setGraphic(lastActionBox);
 		return expirationDateColumn;
 	}
@@ -435,9 +443,10 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 							button.setVisible(!historyData.isExpired());
 							button.setDisable(!historyData.isHistory());
 
-							if (historyData.getType().equals(FileType.ACCOUNT_INFO) ||
-									historyData.getType().equals(FileType.BUNDLE) ||
-									historyData.getType().equals(FileType.PUBLIC_KEY)) {
+							if (ACCOUNT_INFO.equals(historyData.getType()) ||
+									BUNDLE.equals(historyData.getType()) ||
+									PUBLIC_KEY.equals(historyData.getType()) ||
+									SOFTWARE_UPDATE.equals(historyData.getType())) {
 								button.setVisible(historyData.getActions().equals(Actions.DECLINE));
 							}
 							button.setOnAction(actionEvent -> setHistoryDataAction(historyData));
@@ -506,6 +515,9 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 		formatDatePicker(expirationStartDatePicker, expirationEndDatePicker, actionsStartDatePicker,
 				actionsEndDatePicker);
 
+		setupStartEndDates(expirationStartDatePicker, expirationEndDatePicker);
+		setupStartEndDates(actionsStartDatePicker, actionsEndDatePicker);
+
 		feePayerPredicate = historyData -> {
 			final var accounts = parseAccountNumbers(feePayerTextField.getText(), controller.getCurrentNetwork());
 			return accounts.isEmpty() || accounts.contains(Identifier.parse(historyData.getFeePayer()).asAccount());
@@ -525,6 +537,19 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 			return b1 && b2;
 		};
 
+	}
+
+	private void setupStartEndDates(final DatePicker start, final DatePicker end) {
+		end.disableProperty().bind(start.valueProperty().isNull());
+		end.setDayCellFactory(picker -> new DateCell() {
+			@Override
+			public void updateItem(final LocalDate date, final boolean empty) {
+				super.updateItem(date, empty);
+				final var begin = start.getValue();
+				final LocalDate today = LocalDate.now();
+				setDisable(empty || date.compareTo(begin) < 0 || date.compareTo(today) > 0);
+			}
+		});
 	}
 
 	private void setupFilterBoxes() {
@@ -709,7 +734,7 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 	 */
 	private boolean isAllowedFile(final String name) {
 		try {
-			return !FileType.getType(FilenameUtils.getExtension(name)).equals(FileType.UNKNOWN);
+			return !getType(FilenameUtils.getExtension(name)).equals(UNKNOWN);
 		} catch (final HederaClientException e) {
 			return false;
 		}
@@ -931,6 +956,14 @@ public class HistoryPaneController implements GenericFileReadWriteAware {
 						return LocalDate.parse(s, dateFormatter);
 					}
 					return null;
+				}
+			});
+			picker.setDayCellFactory(p -> new DateCell() {
+				@Override
+				public void updateItem(final LocalDate date, final boolean empty) {
+					super.updateItem(date, empty);
+					final LocalDate today = LocalDate.now();
+					setDisable(empty || date.compareTo(today) > 0);
 				}
 			});
 		}
