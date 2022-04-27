b31f967363aa98fc6b470db9dc4942c220c9c1b7
David Sergio Matusevich <davidmatusevich@swirlds.com>
2022-04-27 08:34:30 -0500

    Bumped version
    Signed-off-by: David Sergio Matusevich <davidmatusevich@swirlds.com>

--- tools-integration/src/test/java/com/hedera/hashgraph/client/integration/QueryNetworkTest.java
+++ tools-integration/src/test/java/com/hedera/hashgraph/client/integration/QueryNetworkTest.java
@@ -50,7 +50,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.testfx.api.FxToolkit;
 
@@ -184,7 +183,7 @@ public class QueryNetworkTest extends TestBase implements GenericFileReadWriteAw
 		//sleep(1000000);
 
 
-		accountsPanePage		.requestNewBalance(nickname);
+		accountsPanePage.requestNewBalance(nickname);
 
 		final var newBalance = accountsPanePage.getBalance(nickname);
 		assertEquals(oldBalance, newBalance);
