051eb4b4080ee3df69dd71b4130ebab1ee20a95b
John Bair <117694970+jbair06@users.noreply.github.com>
2023-05-08 10:54:50 -0700

    Update QueryNetworkTest.java
    Fixing environment variable usage
    
    Signed-off-by: John Bair <117694970+jbair06@users.noreply.github.com>

--- tools-integration/src/test/java/com/hedera/hashgraph/client/integration/QueryNetworkTest.java
+++ tools-integration/src/test/java/com/hedera/hashgraph/client/integration/QueryNetworkTest.java
@@ -94,8 +94,9 @@ class QueryNetworkTest extends TestBase implements GenericFileReadWriteAware {
 
 	@BeforeAll
 	static void beforeAll() throws KeyStoreException {
-		final var myAccountId = AccountId.fromString(Dotenv.configure().directory("../").load().get("MY_ACCOUNT_ID"));
-		final var myPrivateKey = PrivateKey.fromString(Dotenv.configure().directory("../").load().get("MY_PRIVATE_KEY"));
+		var dotenv = Dotenv.configure().directory("../").load();
+		final var myAccountId = AccountId.fromString(dotenv.get("TEST_ACCOUNT_ID"));
+		final var myPrivateKey = PrivateKey.fromString(dotenv.get("TEST_PRIVATE_KEY"));
 
 		client = Client.forTestnet();
 		client.setOperator(myAccountId, myPrivateKey);
