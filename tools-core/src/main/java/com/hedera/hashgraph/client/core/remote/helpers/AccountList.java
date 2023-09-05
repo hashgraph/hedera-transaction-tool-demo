169d4a9aea2073f551e962aed07648831c79d82d
John Bair <john.bair@swirldslabs.com>
2023-09-04 22:27:43 -0700

    AccountList is a helper class used for the list of nodes or accounts to be used in a transaction.
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- tools-core/src/main/java/com/hedera/hashgraph/client/core/remote/helpers/AccountList.java
+++ tools-core/src/main/java/com/hedera/hashgraph/client/core/remote/helpers/AccountList.java
@@ -1,21 +1,21 @@
 package com.hedera.hashgraph.client.core.remote.helpers;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class AccountList {
-    private static final String INPUT_STRING = "input";
-    private static final String LIST_STRING = "list";
+    public static final String INPUT_STRING = "input";
+    public static final String LIST_STRING = "list";
 
-    @JsonProperty()
-    private String input;
-    @JsonProperty()
-    private List<String> list;
+    private final String input;
+    private final List<String> list;
 
     public AccountList(String input, List<String> list) {
         this.input = input;
@@ -33,5 +33,27 @@ public class AccountList {
 
     public List<String> getList() {
         return list;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append(INPUT_STRING, input)
+                .append(LIST_STRING, list)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof AccountList) {
+            return Objects.equals(((AccountList) obj).list, list)
+                    && Objects.equals(((AccountList) obj).input, input);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return input.hashCode() + list.hashCode();
     }
 }
