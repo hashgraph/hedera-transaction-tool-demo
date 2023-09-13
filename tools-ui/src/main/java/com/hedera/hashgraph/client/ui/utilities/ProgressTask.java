eca6b45539f58a45c30165e8520424d92368f635
John Bair <117694970+jbair06@users.noreply.github.com>
2023-09-13 14:42:53 -0700

    505 creating a testnet account freezes (#507)
    Signed-off-by: John Bair <john.bair@swirldslabs.com>

--- tools-ui/src/main/java/com/hedera/hashgraph/client/ui/utilities/ProgressTask.java
+++ tools-ui/src/main/java/com/hedera/hashgraph/client/ui/utilities/ProgressTask.java
@@ -4,4 +4,6 @@ import javafx.concurrent.Task;
 
 public abstract class ProgressTask<T> extends Task<T> {
     public abstract String getDescription();
+    public abstract Object getResult();
+    public abstract void setResult(final Object results);
 }
